4e17f17191e7101ed3d9b194b08e6036dc3bebed
Refayat Haque <refayathaque@gmail.com>
2019-06-05 21:24:19 -0400

    ip

--- src/ResourceListWithHooks.js
+++ src/ResourceListWithHooks.js
@@ -14,10 +14,11 @@ const ResourceListWithHooks = ({ resource }) => {
   useEffect(() => {
     fetchResource(resource)
   }, [resource]);
-  // ^ `useEffect` is being called every time this component is being rendered, between subsquent renders of this componeent, if the elements in the array (second arguyment in `useEffect`) is differeent, the arrow function we passed in (first argument in `useEffect`) is going to be called
-  // The default value for 'resoource' coming in as props from the parent component is defaulted to 'posts' (initial state in the App component), when we click on 'todos' in the App component then this componeent is re-rendered and the first element inside ^ array is 'todos', since 'todos' is diffeernt from 'posts', the arrow function in `useEffect` gets called
-  // If we call `useEffect` twice in a row with the same element inside ^ array (click on 'posts'/'todos' twice ein the App component), the arrow function will not get called because that element has not changed
-  // We are essentially doing what we did in line 15 of ResourceList
+  // ^ `useEffect` is being called every time this component is rendered, between subsquent renders of this component, if the element in the array (second argument in `useEffect`) is a different value, the arrow function we passed in (first argument in `useEffect`) is going to be called
+  // The default value for 'resoource' coming in to this component as props from the parent component is defaulted to 'posts' (as per the initial state in the App component), and when we click on 'todos' in the App component then this componeent is re-rendered with the first element inside ^ array being 'todos'
+  // ^ Since 'todos' is different to 'posts', the arrow function in `useEffect` gets called
+  // If we call `useEffect` more than once with the same element inside ^ array (say you click on 'posts'/'todos' twice/thrice/etc., in the App component), the arrow function in `useEffect` will not get called because that array element has not changed
+  // Here we are essentially doing what we did in line 15 block of the ResourceList class-based component
 
   return (
     <div>
@@ -34,5 +35,5 @@ export default ResourceListWithHooks;
 // Notes:
 
 // `useEffect` - Allows a functional component to use 'lifecycle methods'
-// ^ Essentially works as a combination of `componentDidMount` and `componentDidUpdate`, `useEffect` will run when the component is first rendereed to the screen (mounted) and anytime the component is re-rendered after that first time
+// ^ Works as a combination of `componentDidMount` and `componentDidUpdate`, `useEffect` will run when the component is first rendereed to the screen (mounted) and also anytime the component is re-rendered after that first time
 // `useState` - Allows a functional component to use component-level state
